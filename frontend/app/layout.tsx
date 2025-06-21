ade762299f7b7417e0ae5fc41b4c0a2480db80c2
Mohammed Afjal Khan <46417210+Azzukhan@users.noreply.github.com>
2025-06-21 04:57:17 +0100

    fix docker build for nextjs

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,11 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "@/components/header"
 import Footer from "../components/footer"
 import { Toaster } from "@/components/ui/sonner"
 
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "UK Water Tracker",
@@ -22,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Header />
           <main>{children}</main>
