a5b9bb9ee1feea7854aa62bf40d461de628598ed
Mohammed Afjal Khan <46417210+Azzukhan@users.noreply.github.com>
2025-06-22 12:50:10 +0100

    Add Celery config env variables

--- backend/uk_water_tracker/celery.py
+++ backend/uk_water_tracker/celery.py
@@ -47,8 +47,8 @@ app.conf.beat_schedule = {
 
 # Configure Celery to use Redis
 app.conf.update(
-    broker_url='redis://localhost:6379/0',
-    result_backend='redis://localhost:6379/0',
+    broker_url=os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0'),
+    result_backend=os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'),
     broker_transport='redis',
     broker_connection_retry_on_startup=True,
     broker_transport_options={
