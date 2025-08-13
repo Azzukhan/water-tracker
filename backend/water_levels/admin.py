b5b39cf35fc46b526bf13a7b941fbc2483ba20d0
Mohammed Afjal Khan <46417210+Azzukhan@users.noreply.github.com>
2025-08-13 02:04:09 +0100

    Register all water level models in admin

--- backend/water_levels/admin.py
+++ backend/water_levels/admin.py
@@ -58,11 +58,64 @@ class SevernTrentReservoirForecastAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(YorkshireWaterPrediction)
-admin.site.register(YorkshireReservoirData)
-admin.site.register(SouthernWaterReservoirLevel)
-admin.site.register(SouthernWaterReservoirForecast)
-admin.site.register(EAwaterStation)
+@admin.register(YorkshireWaterPrediction)
+class YorkshireWaterPredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "predicted_reservoir_percent",
+        "predicted_demand_mld",
+        "model_type",
+    )
+    list_filter = ("model_type",)
+
+
+@admin.register(YorkshireReservoirData)
+class YorkshireReservoirDataAdmin(admin.ModelAdmin):
+    list_display = (
+        "report_date",
+        "reservoir_level",
+        "weekly_difference",
+        "direction",
+    )
+
+
+@admin.register(SouthernWaterReservoirLevel)
+class SouthernWaterReservoirLevelAdmin(admin.ModelAdmin):
+    list_display = (
+        "reservoir",
+        "date",
+        "current_level",
+        "average_level",
+        "change_week",
+        "change_month",
+        "difference_from_average",
+    )
+    list_filter = ("reservoir",)
+
+
+@admin.register(SouthernWaterReservoirForecast)
+class SouthernWaterReservoirForecastAdmin(admin.ModelAdmin):
+    list_display = (
+        "reservoir",
+        "date",
+        "predicted_level",
+        "model_type",
+    )
+    list_filter = ("reservoir", "model_type")
+
+
+@admin.register(EAwaterStation)
+class EAwaterStationAdmin(admin.ModelAdmin):
+    list_display = (
+        "station_id",
+        "name",
+        "region",
+        "latitude",
+        "longitude",
+    )
+    list_filter = ("region",)
+
+
 @admin.register(EAwaterLevel)
 class EAwaterLevelAdmin(admin.ModelAdmin):
     list_display = (
@@ -74,12 +127,113 @@ class EAwaterLevelAdmin(admin.ModelAdmin):
     ordering = ("-date",)  # Show most recent records first
     list_filter = ("station", "quality")
 
-admin.site.register(EAwaterPrediction)
-admin.site.register(EAwaterPredictionAccuracy)
-admin.site.register(SevernTrentForecastAccuracy)
-admin.site.register(YorkshireWaterPredictionAccuracy)
-admin.site.register(SouthernWaterForecastAccuracy)
-admin.site.register(ScottishWaterPredictionAccuracy)
-admin.site.register(ScottishWaterForecast)
-admin.site.register(ScottishWaterRegionalForecast)
-admin.site.register(ScottishWaterForecastAccuracy)
+
+@admin.register(EAwaterPrediction)
+class EAwaterPredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        "region",
+        "date",
+        "model_type",
+        "predicted_value",
+    )
+    list_filter = ("region", "model_type")
+
+
+@admin.register(EAwaterPredictionAccuracy)
+class EAwaterPredictionAccuracyAdmin(admin.ModelAdmin):
+    list_display = (
+        "region",
+        "date",
+        "model_type",
+        "predicted_value",
+        "actual_value",
+        "percentage_error",
+    )
+    list_filter = ("region", "model_type")
+
+
+@admin.register(SevernTrentForecastAccuracy)
+class SevernTrentForecastAccuracyAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "model_type",
+        "predicted_percentage",
+        "actual_percentage",
+        "percentage_error",
+    )
+    list_filter = ("model_type",)
+
+
+@admin.register(YorkshireWaterPredictionAccuracy)
+class YorkshireWaterPredictionAccuracyAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "model_type",
+        "predicted_reservoir_percent",
+        "actual_reservoir_percent",
+        "reservoir_error",
+        "predicted_demand_mld",
+        "actual_demand_mld",
+        "demand_error",
+    )
+    list_filter = ("model_type",)
+
+
+@admin.register(SouthernWaterForecastAccuracy)
+class SouthernWaterForecastAccuracyAdmin(admin.ModelAdmin):
+    list_display = (
+        "reservoir",
+        "date",
+        "model_type",
+        "predicted_level",
+        "actual_level",
+        "percentage_error",
+        "forecast_date",
+    )
+    list_filter = ("reservoir", "model_type")
+
+
+@admin.register(ScottishWaterPredictionAccuracy)
+class ScottishWaterPredictionAccuracyAdmin(admin.ModelAdmin):
+    list_display = (
+        "area",
+        "date",
+        "model_type",
+        "predicted_value",
+        "actual_value",
+        "percentage_error",
+    )
+    list_filter = ("area", "model_type")
+
+
+@admin.register(ScottishWaterForecast)
+class ScottishWaterForecastAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "predicted_percentage",
+        "model_type",
+    )
+    list_filter = ("model_type",)
+
+
+@admin.register(ScottishWaterRegionalForecast)
+class ScottishWaterRegionalForecastAdmin(admin.ModelAdmin):
+    list_display = (
+        "area",
+        "date",
+        "predicted_level",
+        "model_type",
+    )
+    list_filter = ("area", "model_type")
+
+
+@admin.register(ScottishWaterForecastAccuracy)
+class ScottishWaterForecastAccuracyAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "model_type",
+        "predicted_percentage",
+        "actual_percentage",
+        "percentage_error",
+    )
+    list_filter = ("model_type",)
