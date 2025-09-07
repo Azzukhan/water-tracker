044c530c70b3eb3fb343777bc4e7677d7d2eda55
Mohammed Afjal Khan <46417210+Azzukhan@users.noreply.github.com>
2025-09-07 20:25:15 +0100

    changes

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
   typeof window !== 'undefined'
     ? process.env.NEXT_PUBLIC_API_URL ||
-      `${window.location.protocol}//${window.location.hostname}:8080`
-    : process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8080';
+      `${window.location.protocol}//${window.location.hostname}:8000`
+    : process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
