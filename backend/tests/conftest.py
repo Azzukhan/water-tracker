ddaf313272de22bb4085f703ba11fb5b013161bf
Mohammed Afjal Khan <46417210+Azzukhan@users.noreply.github.com>
2025-09-08 08:14:10 +0100

    changes

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,7 @@
-import os
-import sys
+# backend/tests/conftest.py
 import pytest
-from rest_framework.test import APIClient
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture
 def api_client():
+    from rest_framework.test import APIClient 
     return APIClient()
